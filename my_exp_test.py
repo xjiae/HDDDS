4518c27a1a201a4abdd61c84fa5cbc9fc464b638
Anton Xue <antonxue@seas.upenn.edu>
2023-06-03 18:50:54 -0400

    fix a ffres torch.cat bug

--- my_exp_test.py
+++ my_exp_test.py
@@ -10,6 +10,9 @@ mvtec_dataset = MVTecDataset("bottle")
 simple_net = SimpleNet(in_shape=(3,256,256), out_shape=(2,))
 ffres = MyFastResA(return_mode="two_class")
 
+# model = simple_net
+model = ffres
+
 grad_configs = GradConfigs()
 intg_configs = IntGradConfigs()
 lime_configs = LimeConfigs(x_train=torch.rand(100,3,256,256))
@@ -20,5 +23,8 @@ dummy_intg_saveto = "saved_explanations/intg_dummy.pt"
 dummy_lime_saveto = "saved_explanations/lime_dummy.pt"
 dummy_shap_saveto = "saved_explanations/shap_dummy.pt"
 
-grad_stuff = get_mvtec_explanation(simple_net, mvtec_dataset, grad_configs, saveto=dummy_grad_saveto, num_todo=50)
+grad_stuff = get_mvtec_explanation(model, mvtec_dataset, grad_configs, saveto=dummy_grad_saveto, num_todo=25)
+intg_stuff = get_mvtec_explanation(model, mvtec_dataset, intg_configs, saveto=dummy_intg_saveto, num_todo=25)
+lime_stuff = get_mvtec_explanation(model, mvtec_dataset, lime_configs, saveto=dummy_lime_saveto, num_todo=25)
+shap_stuff = get_mvtec_explanation(model, mvtec_dataset, shap_configs, saveto=dummy_shap_saveto, num_todo=25)
 

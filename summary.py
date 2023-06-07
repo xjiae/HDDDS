7f115bb90bc38048eadecd2a9dc797d268aa76a3
xjiae <xjiae@seas.upenn.edu>
2023-06-07 14:46:05 -0400

    cleaning

--- summary.py
+++ summary.py
@@ -3,6 +3,7 @@ import torch
 import os
 import numpy as np
 from collections import defaultdict
+from tqdm import tqdm
 PT_FOLDER = "/data1/antonxue/xjiae/anton-files/saved_explanations"
 
 def evaluate(ret):
@@ -32,13 +33,15 @@ def get_table(ds_name, model, attr):
     for file in os.listdir(PT_FOLDER):
         if ds_name in file and model in file and attr in file:
             fp = os.path.join(PT_FOLDER, file)
-            f = open(fp, "rb")
-            ret = torch.load(f)
-            acc, f1, fpr, fnr = evaluate(ret)
-            rets['fpr'].append(fpr)
-            rets['fnr'].append(fnr)
-            rets['acc'].append(acc)
-            rets['f1'].append(f1)
+            if os.path.getsize(fp) > 0:  
+                print(file)
+                f = open(fp, "rb")
+                ret = torch.load(f)
+                acc, f1, fpr, fnr = evaluate(ret)
+                rets['fpr'].append(fpr)
+                rets['fnr'].append(fnr)
+                rets['acc'].append(acc)
+                rets['f1'].append(f1)
     compute_stat(rets, model, ds_name, attr)
 
 def compute_overall_avg(ds_name, attr):
@@ -46,19 +49,58 @@ def compute_overall_avg(ds_name, attr):
     for file in os.listdir(PT_FOLDER):
         if ds_name in file and attr in file:
             fp = os.path.join(PT_FOLDER, file)
-            f = open(fp, "rb")
-            ret = torch.load(f)
-            acc, f1, fpr, fnr = evaluate(ret)
-            rets['fpr'].append(fpr)
-            rets['fnr'].append(fnr)
-            rets['acc'].append(acc)
-            rets['f1'].append(f1)
-    ret = compute_stat(rets, None, ds_name, attr)    
+            if os.path.getsize(fp) > 0:  
+                print(file)
+                f = open(fp, "rb")
+                ret = torch.load(f)
+                acc, f1, fpr, fnr = evaluate(ret)
+                rets['fpr'].append(fpr)
+                rets['fnr'].append(fnr)
+                rets['acc'].append(acc)
+                rets['f1'].append(f1)
+    ret = compute_stat(rets, None, ds_name, attr, False)    
     return ret    
     
     
 
+
+def all():
+    all = tabular + image + text
+    # all = tabular
+    
+    for attr in attr_models:
+        rets = defaultdict(list)
+        for ds_name in all:
+            fpr, fpr_std, fnr, fnr_std, acc, acc_std, f1, f1_std = compute_overall_avg(ds_name, attr)
+            rets['fpr'].append(fpr)
+            rets['fnr'].append(fnr)
+            rets['acc'].append(acc)
+            rets['f1'].append(f1)
+            rets['fpr_std'].append(fpr_std)
+            rets['fnr_std'].append(fnr_std)
+            rets['acc_std'].append(acc_std)
+            rets['f1_std'].append(f1_std)
+            
+        saveto = open(f"results/results_all.txt", "a")
+        saveto.write(f"{attr}\n")
+        saveto.write(f"fpr: {rets['fpr']}\n")
+        saveto.write(f"fpr_std: {rets['fpr_std']}\n")
+        saveto.write(f"fnr: {rets['fnr']}\n")
+        saveto.write(f"fnr_std: {rets['fnr_std']}\n")
+        saveto.write(f"acc: {rets['acc']}\n")
+        saveto.write(f"acc_std: {rets['acc_std']}\n")
+        saveto.write(f"f1: {rets['f1']}\n")
+        saveto.write(f"f1_std: {rets['f1_std']}\n")
+        saveto.write(f"fpr_avg: {np.mean(rets['fpr']):.4f}\n")
+        saveto.write(f"fnr_avg: {np.mean(rets['fnr']):.4f}\n")
+        saveto.write(f"acc_avg: {np.mean(rets['acc']):.4f}\n")
+        saveto.write(f"f1_avg: {np.mean(rets['f1']):.4f}\n")
+        saveto.close()
+
+
+
 attr_models = ["grad", "intg"]
+# , "swat", "wadi"
 tabular = ["hai", "swat", "wadi"]
 image = ["mvtec"]
 text = ["squad"]
@@ -67,14 +109,9 @@ tabular_models = ['lstm', 'lr']
 image_models = ["ffres"]
 text_models = ["roberta"]
 
-def all():
-    all = tabular + image + text
-    for attr in attr_models:
-        for ds_name in all:
-            get
-
 def hai():
-    for ds_name in tabular:
+    for ds_name in tqdm(tabular):
+        print(ds_name)
         for model in tabular_models:
             for attr in attr_models:
                 get_table(ds_name, model, attr)
@@ -94,4 +131,4 @@ def squad():
                 print(f"running {attr}...")
                 get_table(ds_name, model, attr)
 
-squad()
+all()

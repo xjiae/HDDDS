ae1f17d2fbcb2c973d6af0b47a7f90e728f98801
AntonXue <antonxue@seas.upenn.edu>
2023-06-10 19:11:33 -0400

    some model updates

--- models/models.py
+++ models/models.py
@@ -14,32 +14,24 @@ from transformers.modeling_outputs import QuestionAnsweringModelOutput
 
 from .fastflow import *
 
-# The two-argument model
-class XwModel(nn.Module):
-  def __init__(self, in_shape, out_shape, w_shape=None):
-    super(XwModel, self).__init__()
+
+class ShapedModel(nn.Module):
+  def __init__(self, in_shape, out_shape):
+    super(ShapedModel, self).__init__()
     self.in_shape = in_shape
     self.out_shape = out_shape
-    self.w_shape = in_shape if w_shape is None else w_shape
     self.in_dim = math.prod(list(in_shape))
     self.out_dim = math.prod(list(out_shape))
-    self.w_dim = math.prod(list(w_shape))
-
-  def forward(self, x, w):
-    raise NotImplementedError()
 
 
-# A simple conv + feed forward network
-class SimpleNet(XwModel):
-  def __init__(self, in_shape, out_shape,
-               linear_dim = 128,
-               auto_reshape = True,
-               softmax_output = True):
-    super(SimpleNet, self).__init__(in_shape, out_shape, w_shape=in_shape)
+class SimpleNet(ShapedModel):
+  def __init__(self, in_shape, out_shape, linear_dim=128, softmax_output=True):
+    super(SimpleNet, self).__init__(in_shape, out_shape)
     self.linear_dim = linear_dim
+    self.softmax_output = softmax_output
     self.norm1 = nn.LayerNorm(self.in_dim)
     self.convs = nn.Sequential(
-      nn.Conv1d(2, 64, 5, stride=1, padding=2),
+      nn.Conv1d(1, 64, 5, stride=1, padding=2),
       nn.ReLU(),
       nn.Conv1d(64, 32, 5, stride=1, padding=2),
       nn.Flatten(1),
@@ -55,126 +47,49 @@ class SimpleNet(XwModel):
       nn.Linear(linear_dim, self.out_dim)
     )
 
-    self.auto_reshape = auto_reshape
-    self.softmax_output = softmax_output
+  def forward(self, x):
+    N = x.size(0)
+    x = x.view(N, *self.in_shape)
 
-  def forward(self, x, w=None):
-    N = x.size(0) # N is batch dim
-    w = torch.ones(N, *self.w_shape).to(x.device) if w is None else w
-    # Reshape as necessary
-    if self.auto_reshape:
-      x = x.view(N, *self.in_shape)
-      w = w.view(N, *self.w_shape)
-
-    x = self.norm1(x.view(N,1,self.in_dim))
-    w = w.view(N,1,self.in_dim)
-    z = torch.cat([x, w], dim=1)
-    z = self.convs(z)   # (N,linear_dim)
+    z = x.view(N,1,self.in_dim)
+    z = self.norm1(z)
+    z = self.convs(z)   # (n, linear_dim)
     z = self.norm2(z)
     z = self.linears(z) # (N,out_dim)
 
-    # If it's classification we do a normalization
     if self.softmax_output:
       z = torch.sigmoid(z) if z.size(1) == 1 else z.softmax(dim=1)
 
-    return z.view(N, *self.out_shape)
+    return z.view(N,*self.out_shape)
 
-class LogisticRegression(XwModel):
-   def __init__(self, in_shape, return_mode="scalar_score"):
-    super(LogisticRegression, self).__init__(in_shape, (1,), w_shape=in_shape)
-    self.linear = torch.nn.Linear(self.in_dim, 1)
-    self.return_mode = return_mode
 
-   def forward(self, x, w=None):
-    x = x.flatten(1).float()
-    outputs = torch.sigmoid(self.linear(x))
-    if self.return_mode == "scalar_score":
-      return outputs  # anom prob
-    elif self.return_mode == "two_class":
-      return torch.cat([1-outputs, outputs], dim=1) # good prob, anom prob
-    else:
-      raise NotImplementedError()
-       
-# A simple LSTM implementation
-class SimpleLSTM(XwModel):
-  def __init__(self, in_shape, out_shape,
-               hidden_dim = 128,
-               auto_reshape = True,
-               return_mode = "last",
-               softmax_output = True):
-    super(SimpleLSTM, self).__init__(in_shape, out_shape, w_shape=in_shape)
-    self.hidden_dim = hidden_dim
-    self.lstm1 = nn.LSTM(input_size=2*self.in_dim, hidden_size=hidden_dim, batch_first=True)
-    self.lstm2 = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, batch_first=True)
-    self.lstm3 = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, batch_first=True)
-    self.linear = nn.Linear(hidden_dim, self.out_dim)
-    self.auto_reshape = auto_reshape
-    self.return_mode = return_mode
-    self.softmax_output = softmax_output
+# Logistic regression
+class LogisticRegression(ShapedModel):
+   def __init__(self, in_shape):
+    super(LogisticRegression, self).__init__(in_shape, (2,))
+    self.linear = torch.nn.Linear(self.in_dim, 2)
 
-  def forward(self, x, w=None):
-    N = x.size(0) # N is batch dim
-    L = x.numel() // (N * self.in_dim) # Horizon
-    w = torch.ones(N, L, *self.w_shape).to(x.device) if w is None else w
-    if self.auto_reshape:
-      x = x.view(N, L, *self.in_shape)
-      w = w.view(N, L, *self.w_shape)
-
-    z = torch.cat([x.flatten(2), w.flatten(2)], dim=2) # (N,L,2*d)
-    z = z.type(torch.FloatTensor).to(x.device) # Force a convert because nn.LSTM is a coward
-    z, _ = self.lstm1(z)
-    z, _ = self.lstm2(z)
-    z, _ = self.lstm3(z)
+   def forward(self, x):
+    z = x.flatten(1).float()
     z = self.linear(z)
-    z = z.view(N,L,*self.out_shape)
+    y = torch.softmax(z, dim=1) # 0: good prob, 1: anom prob
+    return y
+   
 
-    if self.softmax_output:
-      z = torch.sigmoid(z) if z.size(2) == 1 else z.softmax(dim=2)
-
-    if self.return_mode == "last":
-      return z[:,-1]
-    elif self.return_mode == "all":
-      return z
-    elif self.return_mode == "prob":
-      max = torch.max(z.clamp(0,1), dim=3)[0]
-      return torch.hstack((max, 1-max)).squeeze()
-    elif self.return_mode == "mean":
-      max = torch.mean(z, dim=3)[0]
-      return max
-    elif self.return_mode == "two_class":
-      assert self.softmax_output and z.size(2) == 1
-      anom_prob = z[:,-1]
-      good_prob = 1 - anom_prob
-      return  torch.cat([good_prob, anom_prob], dim=1) #(N, 2)
-    
-    else:
-      raise NotImplementedError()
-
-
-# x --[fastflow]--> (x, hmap, w) --[resnet]--> [0,1]
-class MyFastResA(XwModel):
+# x --[fastflow]--> (x, hmap, w) --[resnet]--> (N,2)
+class MyFastResA(ShapedModel):
   def __init__(self,
                in_shape = (3,256,256),
-               w_shape = (1,256,256),
                backbone_name = "resnet18",
                flow_steps = 8,
                conv3x3_only = False,
                freeze_fastflow = False,
-               auto_reshape = True,
-               return_mode = "scalar_score"):
-      if return_mode == "scalar_score":
-        out_shape = (1,)
-      elif return_mode == "two_class":
-        out_shape = (2,)
-      elif return_mode == "all":
-        out_shape = (1,)
-      else:
-        raise NotImplementedError()
-      super(MyFastResA, self).__init__(in_shape, out_shape, w_shape=w_shape)
+               return_mode = "two_class"):
+      super(MyFastResA, self).__init__(in_shape, (2,))
       self.backbone_name = backbone_name
       self.ffmodel = FastFlow(backbone_name, flow_steps, in_shape[1], conv3x3_only)
 
-      assert len(in_shape) == 3 and len(w_shape) == 3
+      assert len(in_shape) == 3
       self.input_channels = in_shape[0]
 
       # Freeze fast flow weights
@@ -183,36 +98,63 @@ class MyFastResA(XwModel):
           param.requires_grad = False
 
       resnet = torchvision.models.resnet18()
-      resnet.conv1 = nn.Conv2d(self.input_channels+2, 64, kernel_size=7, stride=2, padding=3)
+      resnet.conv1 = nn.Conv2d(self.input_channels+1, 64, kernel_size=7, stride=2, padding=3)
       self.resnet = resnet
-      self.hmap_shape = w_shape
-      self.auto_reshape = auto_reshape
       self.return_mode = return_mode
 
-  def forward(self, x, w=None):
+  def forward(self, x):
     N = x.size(0) # N is batch dim
-    w = torch.ones(N, *self.w_shape).to(x.device) if w is None else w
-    # Reshape as necessary
-    if self.auto_reshape:
-      x = x.view(N, *self.in_shape)
-      w = w.view(N, *self.w_shape)
-
+    x = x.view(N, *self.in_shape)
     ret = self.ffmodel(x)
     hmap = ret["anomaly_map"]
-    z = self.resnet(torch.cat([x, hmap, w], dim=1))
-    z = torch.sigmoid(z[:,0]).view(-1,1)
-    ret['anomaly_score'] = z
-
-    if self.return_mode == "scalar_score":
-      return z  # (N,1)
-    elif self.return_mode == "two_class":
-      anom_prob = z
-      good_prob = 1 - anom_prob
-      return torch.cat([good_prob, anom_prob], dim=1) # (N,2)
+    z = self.resnet(torch.cat([x, hmap], dim=1))
+    z = z[:,0:2]
+    z = torch.softmax(z, dim=1)
+    ret['anomaly_score'] = z[:,1]
+    if self.return_mode == "two_class":
+      return z
     else:
       return ret
 
+    
+# A simple LSTM implementation
+class SimpleLSTM(ShapedModel):
+  def __init__(self, in_shape,
+               hidden_dim = 128,
+               auto_reshape = True,
+               return_mode = "last"):
+    super(SimpleLSTM, self).__init__(in_shape, (2,))
+    self.hidden_dim = hidden_dim
+    self.lstm1 = nn.LSTM(input_size=self.in_dim, hidden_size=hidden_dim, batch_first=True)
+    self.lstm2 = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, batch_first=True)
+    self.lstm3 = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, batch_first=True)
+    self.linear = nn.Linear(hidden_dim, self.out_dim)
+    self.return_mode = return_mode
 
+  def forward(self, x):
+    N = x.size(0) # N is batch dim
+    L = x.numel() // (N * self.in_dim) # Horizon
+
+    z = x.view(N,L,self.in_dim).float()
+    z, _ = self.lstm1(z)
+    z, _ = self.lstm2(z)
+    z, _ = self.lstm3(z)
+    z = self.linear(z)    # (N,L,2)
+    z = z.softmax(dim=2)
+
+    if self.return_mode == "last":
+      return z[:,-1]  # (N,2)
+    elif self.return_mode == "all":
+      return z        # (N,L,2)
+    elif self.return_mode == "exists":
+      anom_probs = z[:,:,1]  # (N,L)
+      max_probs = anom_probs.max(dim=1).values.view(N,1)
+      return torch.cat([1-max_probs, max_probs], dim=1)  # (N,2)
+    else:
+      raise NotImplementedError()
+
+
+'''
 #
 class MySquadModel(XwModel):
   def __init__(self,
@@ -275,4 +217,5 @@ class MySquadModel(XwModel):
     else:
       raise NotImplementedError()
 
+'''
 

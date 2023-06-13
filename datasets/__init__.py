0456547ddf62231bf1d3ac227a5c3f5bd4d4a104
xjiae <xjiae@seas.upenn.edu>
2023-06-13 17:41:22 -0400

    set default batch size to be 4

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -5,22 +5,22 @@ from .squad import *
 from .timeseries import *
 
 
-def get_data_bundle(dataset_name, **kwargs):
+def get_data_bundle(dataset_name, train_batch_size=4, test_batch_size=4, **kwargs):
     dataset_name = dataset_name.lower()
     if dataset_name == "mvtec":
-        return get_mvtec_bundle(**kwargs)
+        return get_mvtec_bundle(train_batch_size=train_batch_size, test_batch_size=test_batch_size, **kwargs)
 
     elif dataset_name == "squad":
-        return get_squad_bundle(**kwargs)
+        return get_squad_bundle(train_batch_size=train_batch_size, test_batch_size=test_batch_size, **kwargs)
 
     elif dataset_name == "hai":
-      return get_timeseries_bundle("hai", **kwargs)
+      return get_timeseries_bundle("hai", train_batch_size=train_batch_size, test_batch_size=test_batch_size, **kwargs)
 
     elif dataset_name == "swat":
-      return get_timeseries_bundle("swat", **kwargs)
+      return get_timeseries_bundle("swat", train_batch_size=train_batch_size, test_batch_size=test_batch_size, **kwargs)
 
     elif dataset_name == "wadi":
-      return get_timeseries_bundle("wadi", **kwargs)
+      return get_timeseries_bundle("wadi", train_batch_size=train_batch_size, test_batch_size=test_batch_size, **kwargs)
 
     else:
         raise NotImplementedError()

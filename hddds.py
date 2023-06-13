a2e30f635a50486d2caf42bc2ff056196bdb97c1
xjiae <xjiae@seas.upenn.edu>
2023-06-13 16:40:46 -0400

    code for running all experiments

--- hddds.py
+++ hddds.py
@@ -7,6 +7,7 @@ import torch
 from datasets import *
 from train import *
 from get_explanations import *
+from summary import *
 
 torch.manual_seed(1234)
 
@@ -18,11 +19,23 @@ def train_all_sample_models():
     train_sample_model("mvtec")
     train_sample_model("squad")
   
-def generate_all_sample_explanations(seeds=range(50), num_todo=100):
+def generate_all_sample_explanations(seeds=range(25), num_todo=100):
     generate_explanations_sample_mvtec(seeds, num_todo=num_todo)
     generate_explanations_sample_timeseries("swat", seeds, num_todo=num_todo)
     generate_explanations_sample_timeseries("hai", seeds, num_todo=num_todo)
     generate_explanations_sample_timeseries("wadi", seeds, num_todo=num_todo)
+    generate_explanations_sample_squad(seeds, num_todo=num_todo)
+    
+
+def evaluate_all():
+    timeseries()
+    mvtec()
+    squad()
+    
+if __name__ == "__main__":
+    train_all_sample_models()
+    generate_all_sample_explanations()
+    evaluate_all()
     
 
 
